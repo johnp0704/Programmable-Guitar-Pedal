ace7c09a8646d2b5451ede992ce7109498667527
John Poirier <jlpoirie@uvm.edu>
2024-11-19 17:14:18 -0500

    Audio Passthrough V1.0
    Beginnings of Audio Passthrough

--- Audio_Passthrough_V1/Audio_Passthrough_V1.ino
+++ Audio_Passthrough_V1/Audio_Passthrough_V1.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
+const int inputPin = A0;  // Analog input from guitar
+const int outputPin = 9; // PWM output to amp
 
+void setup() {
+    pinMode(outputPin, OUTPUT); //Set pin mode
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    int sensorValue = analogRead(inputPin); //Read signal from guitar
+    int outputValue = map(sensorValue, 0, 1023, 0, 255); // Map to 8-bit PWM
+    analogWrite(outputPin, outputValue); //Output to amp
 }
